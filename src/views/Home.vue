57e4ab562d90356e1dfcd0642afdb9f12bef4da1
Ted <tugrulerdemdogru@gmail.com>
2021-09-09 15:21:34 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+  
+    <HelloWorld />
+
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
